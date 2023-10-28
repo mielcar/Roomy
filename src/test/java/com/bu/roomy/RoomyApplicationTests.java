e3f9254bb48854740fcd479c680b5d7b63a139e2
Tesseract <pmielcar@gmail.com>
2023-10-28 13:55:06 +0300

    Initial commit

---
+++ src/test/java/com/bu/roomy/RoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.roomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
