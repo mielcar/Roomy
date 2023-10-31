f36acbc76483922e3e51abaacd955a5184bdbbb4
Tesseract <pmielcar@gmail.com>
2023-10-31 13:51:00 +0200

    Swagger

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testing playground
+This project has got swagger dependencies included - most effective way to test it, is to run 
+RoomyApplication in your IDE and access http://localhost:8080/swagger-ui.html
--- src/main/java/com/bu/roomy/application/BookingOptimizationController.java
+++ src/main/java/com/bu/roomy/application/BookingOptimizationController.java
@@ -3,12 +3,15 @@ package com.bu.roomy.application;
 import com.bu.roomy.domain.BookingsOptimizer;
 import com.bu.roomy.domain.OptimizedBookings;
 import com.bu.roomy.domain.RoomsDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Bookings", description = "Bookings statistics API")
 @RequestMapping(path = "/bookings")
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class BookingOptimizationController {
 
     private final BookingsOptimizer bookingsOptimizer;
 
+    @Operation(summary = "Calculate optimized rooms occupancy")
     @GetMapping(path = "/optimization")
     OptimizedBookings optimize(@RequestParam int economyRooms,
                                @RequestParam int premiumRooms,
